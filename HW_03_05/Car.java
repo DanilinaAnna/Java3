423250317e688f31be88a2fc0d7daca27d52fee7
DanilinaAnna <anna.danilina.job@gmail.com>
2021-12-04 20:23:00 +0300

    HW03_05 end

--- HW_03_05/Car.java
+++ HW_03_05/Car.java
@@ -1,34 +1,56 @@
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.atomic.AtomicBoolean;
+
 public class Car implements Runnable {
     private static int CARS_COUNT;
+    private static AtomicBoolean ab = new AtomicBoolean(false);
     static {
         CARS_COUNT = 0;
     }
+    CyclicBarrier carBarrier;
     private Race race;
     private int speed;
     private String name;
+
+    public Car(Race race, int speed, CyclicBarrier carBarrier) {
+        this.race = race;
+        this.speed = speed;
+        this.carBarrier = carBarrier;
+        CARS_COUNT++;
+        this.name = "Участник #" + CARS_COUNT;
+    }
+
     public String getName() {
         return name;
     }
     public int getSpeed() {
         return speed;
     }
+    /*
     public Car(Race race, int speed) {
         this.race = race;
         this.speed = speed;
         CARS_COUNT++;
         this.name = "Участник #" + CARS_COUNT;
     }
+    */
     @Override
     public void run() {
         try {
             System.out.println(this.name + " готовится");
-            Thread.sleep(500 + (int)(Math.random() * 800));
+            Thread.sleep(500 + (int) (Math.random() * 800));
             System.out.println(this.name + " готов");
+            carBarrier.await();
+            carBarrier.await();
+            for (int i = 0; i < race.getStages().size(); i++) {
+                race.getStages().get(i).go(this);
+            }
+            if (ab.compareAndSet(false,true)){
+                System.out.println("Car " + name + " Winner");
+            }
+            carBarrier.await();
         } catch (Exception e) {
             e.printStackTrace();
-        }
-        for (int i = 0; i < race.getStages().size(); i++) {
-            race.getStages().get(i).go(this);
         }
     }
 }
--- HW_03_05/Tunnel.java
+++ HW_03_05/Tunnel.java
@@ -1,4 +1,7 @@
+import java.util.concurrent.Semaphore;
+
 public class Tunnel extends Stage {
+    private Semaphore sem = new Semaphore(MainClass.CARS_COUNT/2);
     public Tunnel() {
         this.length = 80;
         this.description = "Тоннель " + length + " метров";
@@ -7,6 +10,7 @@ public class Tunnel extends Stage {
     public void go(Car c) {
         try {
             try {
+                sem.acquire();
                 System.out.println(c.getName() + " готовится к этапу(ждет): " + description);
                 System.out.println(c.getName() + " начал этап: " + description);
                 Thread.sleep(length / c.getSpeed() * 1000);
@@ -14,6 +18,7 @@ public class Tunnel extends Stage {
                 e.printStackTrace();
             } finally {
                 System.out.println(c.getName() + " закончил этап: " + description);
+                sem.release();
             }
         } catch (Exception e) {
             e.printStackTrace();
